c7526f99c33a9c79b3c9de64988cfd11c4af8448
Cody Romano <codyromano@gmail.com>
2025-02-16 22:22:17 -0800

    minor fixes and pipe interaction logic

--- scripts/Hero.gd
+++ scripts/Hero.gd
@@ -7,14 +7,44 @@ extends CharacterBody2D
 var is_facing_right: bool = true
 var spend_fuel_duration: float = 0
 var fuel_to_burn: float = 0
+var tile_map: TileMap
 
 func _ready():
+	tile_map = get_node('/root/Level 1/TileMap')
 	GlobalSignals.update_fuel_remaining.connect(_update_fuel_remaining)
+
+func _detect_pipe_collision():
+	# Detect the tile the player is standing on
+	var tile_pos = tile_map.local_to_map(global_position)
+	var sound: AudioStreamPlayer2D = $SoundPipe
+
+	
+	# TODO: Detect tiles by terrain attributes
+	if (tile_pos == Vector2i(15, -14) || tile_pos == Vector2i(16, -14)):
+		if (!sound.playing):
+			sound.play()
+	
+	
+	# var current_cell_tile_data = tile_map.get_cell_tile_data(0, tile_pos)
+
+
+	# Get the tile ID at the current position
+	# var tile_id = tile_map.get_cell(tile_pos)
+	# print(tile_id)
+	
+	# Check if the tile has a terrain tag
+	#if tile_id != TileMap.INVALID_CELL:
+	#	var terrain_type = $TileMap.tileset.get_terrain_info(tile_id)  # Check terrain
+	#	if terrain_type == "water":
+	#		print("Standing on water terrain")
+
 
 func _update_fuel_remaining(fuel_left):
 	fuel_to_burn = fuel_left
 	
 func _process(delta):
+	self._detect_pipe_collision()
+	
 	var tween: Tween = create_tween()
 	if Input.is_action_pressed("fart"):
 		spend_fuel_duration += delta
